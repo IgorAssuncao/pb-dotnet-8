0428a6b4e594ab8f0cf92234662e8c609d3ba1b2
IgorAssuncao <igor.oprmonde@gmail.com>
2020-08-24 21:25:47 -0300

    fix

--- backend/Hortogram/Hortogram/Startup.cs
+++ backend/Hortogram/Hortogram/Startup.cs
@@ -23,7 +23,6 @@ namespace Hortogram
 {
     public class Startup
     {
-        private readonly 
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
