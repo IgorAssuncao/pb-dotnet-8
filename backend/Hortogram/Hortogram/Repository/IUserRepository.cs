5dc10fa99ceee4e7d26f35ef8062732d4f13c609
IgorAssuncao <igor.oprmonde@gmail.com>
2020-09-20 21:12:00 -0300

    changed more stuff to async

--- backend/Hortogram/Hortogram/Repository/IUserRepository.cs
+++ backend/Hortogram/Hortogram/Repository/IUserRepository.cs
@@ -13,19 +13,12 @@ namespace Repositories
         HortogramContext Context { get; set; }
 
         Task CreateUser(User user);
-
-        void UpdateUser(User user);
-
+        Task UpdateUser(User user);
         Task<User> GetByEmail(string email);
-
         Task<User> GetById(Guid id);
-
         Task<List<User>> GetAll();
-
         Task<List<UserFollowersResponse>> GetFollowers(User user);
-
         Task AddFollower(UsersFollowers userFollower);
-
-        void RemoveFollower(UsersFollowers userFollower);
+        Task RemoveFollower(UsersFollowers userFollower);
     }
 }
