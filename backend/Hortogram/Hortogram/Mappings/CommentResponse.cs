6bd39b6b59b7fa1924f8f3deb8caf4f10f6f759e
Rafael Brito <rafael.souzabrito7@gmail.com>
2020-09-21 01:16:51 -0300

    comment response mapped

---
+++ backend/Hortogram/Hortogram/Mappings/CommentResponse.cs
@@ -0,0 +1,13 @@
+﻿using Models;
+using System;
+
+namespace Services
+{
+    public class PostResponse
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string PhotoUrl { get; set; }
+        public string Description { get; set; }
+    }
+}
